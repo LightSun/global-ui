efa3b1462bf4ade1a7b40637f609ee50f8665914
heaven7 <532278976@qq.com>
2021-03-29 17:16:45 +0800

    wait

---
+++ GlobalUiApp/app/src/main/java/com/heaven7/globalui/app/MainActivity.java
@@ -0,0 +1,16 @@
+package com.heaven7.globalui.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.libsdl.app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
